72f4e5d96a9876b07ecac7e3cbcb34ac0a8d9d81
chen <1371582196@qq.com>
2020-04-28 16:23:17 +0800

    1

--- lodash/huchisama-lodash.js
+++ lodash/huchisama-lodash.js
@@ -1,4 +1,10 @@
 
 var huchisama = {
+  chunk: function (array, size = 1) {
+    let arr = []
+    for (let i = size; i < array.length; i + size) {
+      arr.push(array.slice(0, i))
+    }
 
+  }
 }
